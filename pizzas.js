a5d74f5e653baa5c40957fa962d6d7f0e17d85e3
João Redmerski <119627205+JoaoRedmerski@users.noreply.github.com>
2023-02-01 14:23:23 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,98 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Yamaha R1',
+        img: 'images/moto.png',
+        price: [121000, 123000, 125000],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: ''
+      },
+      {
+        id: 2,
+        name: 'Kawasaki Ninja',
+        img: 'images/moto1.png',
+        price: [121000, 124000, 126000],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: ''
+      },
+      {
+        id: 3,
+        name: 'Yamaha YZF-R3',
+        img: 'images/moto2.png',
+        price: [123000, 126000, 128000],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: ''
+      },
+      {
+        id: 4,
+        name: 'Ducatti Diavel',
+        img: 'images/moto3.png',
+        price: [120000, 122000, 124000],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: ''
+      },
+      {
+        id: 5,
+        name: 'Ducatti Superleggera',
+        img: 'images/moto4.png',
+        price: [124000, 127000, 129000],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: ''
+      },
+      {
+        id: 6,
+        name: 'Suzuki GSXR',
+        img: 'images/moto5.png',
+        price: [130000, 133000, 135000],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: ''
+      },
+      {
+        id: 7,
+        name: 'Suzuki Hayabuza',
+        img: 'images/moto6.png',
+        price: [127000, 130000, 132000],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: ''
+      },
+      {
+        id: 8,
+        name: 'Honda CBR 1000RR',
+        img: 'images/moto7.png',
+        price: [130000, 132000, 135000],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: ''
+      }
+];
